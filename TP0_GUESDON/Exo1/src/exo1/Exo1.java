c854faa3a03b61004cff786852ebcc3747506626
orianne <orianne.guesdon@epfedu.fr>
2023-09-18 12:27:33 +0200

    Exercice 1 du TD0

---
+++ TP0_GUESDON/Exo1/src/exo1/Exo1.java
@@ -0,0 +1,30 @@
+/*
+ * TP0 Exercice 1
+ * GUESDON
+ * Septembre 2023
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ogues
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Bonjour");
+        System.out.println("Au revoir");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Salut ! Quel est ton nom ?");
+        prenom = sc.nextLine();
+    }
+    
+}
