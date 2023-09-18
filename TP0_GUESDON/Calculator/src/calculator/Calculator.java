184aa670000c41b1c528dfcada91f658a8689164
orianne <orianne.guesdon@epfedu.fr>
2023-09-18 12:58:15 +0200

    Exercice 3 - Calculator (petit exercice 1 et 2) du TD0

---
+++ TP0_GUESDON/Calculator/src/calculator/Calculator.java
@@ -0,0 +1,26 @@
+/*
+ * TP0 Calculator
+ * Orianne GUESDON
+ * 18 Septembre 2023
+ */
+package calculator;
+
+/**
+ *
+ * @author ogues
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Please enter the operator:\n" +
+"1) add\n" +
+"2) substract\n" +
+"3) multiply\n" +
+"4) divide\n" +
+"5) modulo");
+    }
+    
+}
