2722252db85eae76cad7a49e6289605bfd7d3ef9
orianne <orianne.guesdon@epfedu.fr>
2023-10-06 17:10:59 +0200

    Exercice 1 - TP2 début

---
+++ TP2_Bieres_Guesdon/src/tp2_bieres_guesdon/TP2_Bieres_Guesdon.java
@@ -0,0 +1,21 @@
+/*
+ * Orianne GUESDON
+ * TP2
+ * 06/10/2023
+ */
+package tp2_bieres_guesdon;
+
+/**
+ *
+ * @author ogues
+ */
+public class TP2_Bieres_Guesdon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BouteilleBiere uneBiere = new BouteilleBiere() ;
+    }
+    
+}
