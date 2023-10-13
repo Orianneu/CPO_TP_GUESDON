1418b2ccc80d48ec880732cc0aba4fafea73d898
orianne <orianne.guesdon@epfedu.fr>
2023-10-13 10:31:44 +0200

    Exercice 1 - TP2

--- TP2_Bieres_Guesdon/src/tp2_bieres_guesdon/BouteilleBiere.java
+++ TP2_Bieres_Guesdon/src/tp2_bieres_guesdon/BouteilleBiere.java
@@ -8,13 +8,41 @@ package tp2_bieres_guesdon;
  *
  * @author ogues
  */
+
 public class BouteilleBiere {
-    String Nom;
-    float degreAlcool;
-    String brasserie;
-    boolean ouverte;
-    public void lireEtiquette() {
-    System.out.println("Bouteille de " + Nom +" (" + degreAlcool +" degres) \nBrasserie : " + brasserie ) ;
+    //Initialisation des variables
+    String Nom; //nom de la biere
+    double degreAlcool; //son degre d'alcool
+    String brasserie; //nom de la brasserie
+    boolean ouverte; //ouverte ou fermée
+    
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        Nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false; //la bouteille est initialement fermée
+    }
+    
+    public void lireEtiquette() { //Affichage de l'étiquette 
+        System.out.println("Bouteille de " + Nom +" (" + degreAlcool +" degres) \nBrasserie : " + brasserie ) ;
+    }
+    
+    public boolean Décapsuler() { //méthode qui vérifie si la bière est ouverte ou non et l'ouvre si pas ouverte
+        if (!ouverte) { //vérification de l'état
+            ouverte = true; //elle est fermée
+            return true; 
+        } else {
+            System.out.println("La bière est déjà ouverte"); //la biere est ouverte donc affiche qu'elle est déjà ouverte
+            return false;
+        }
+    }
+    @Override
+    public String toString() { //affichage de la chaine
+        String chaine_a_retourner;
+        chaine_a_retourner = Nom + " (" + degreAlcool + " degrés) Ouverte ? "; 
+        if (ouverte == true ) chaine_a_retourner += "oui" ;
+        else chaine_a_retourner += "non" ;
+        return chaine_a_retourner ;
     }
 }
 
--- TP2_Bieres_Guesdon/src/tp2_bieres_guesdon/TP2_Bieres_Guesdon.java
+++ TP2_Bieres_Guesdon/src/tp2_bieres_guesdon/TP2_Bieres_Guesdon.java
@@ -15,7 +15,26 @@ public class TP2_Bieres_Guesdon {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere() ;
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0 ,"Dubuisson") ; //création d'un objet de classe BouteilleBiere
+        uneBiere.lireEtiquette(); //Utilisation de la méthode lireEtiquette qui va afficher les données de la bouteilles
+        uneBiere.Décapsuler(); //Utilisation de la méthode Décapsuler qui va vérifier si la biere est deja ouverte, si non, elle l'ouvre.
+
+        BouteilleBiere uneBiere2 = new BouteilleBiere("Biere 2", 8.0 ,"Brasserie1") ;
+        uneBiere2.lireEtiquette();
+        uneBiere2.Décapsuler();
+
+        BouteilleBiere uneBiere3 = new BouteilleBiere("Biere 3", 7.6 ,"Brasserie2") ;
+        uneBiere3.lireEtiquette();
+        uneBiere3.Décapsuler(); //on ouvre la biere
+        uneBiere3.Décapsuler(); //on teste si il y a renvoie de "La biere est déjà ouverte"
+       
+        BouteilleBiere uneBiere4 = new BouteilleBiere("Biere 4", 6.6 ,"Brasserie3") ;
+        uneBiere4.lireEtiquette();
+        uneBiere4.Décapsuler(); //on ouvre la biere
+        uneBiere4.Décapsuler(); //on teste si il y a renvoie de "La biere est déjà ouverte"
+
+        
+
     }
     
 }
